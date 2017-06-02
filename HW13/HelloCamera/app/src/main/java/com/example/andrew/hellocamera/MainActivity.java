ae76bf09f9f908bd20d1d2f0c76affbebba385d2
adai2017 <AndrewDai2017@u.northwestern.edu>
2017-06-02 16:38:41 -0500

    HW13 WIP

--- HW13/HelloCamera/app/src/main/java/com/example/andrew/hellocamera/MainActivity.java
+++ HW13/HelloCamera/app/src/main/java/com/example/andrew/hellocamera/MainActivity.java
@@ -37,6 +37,12 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
     private Paint paint1 = new Paint();
     private TextView mTextView;
 
+    SeekBar myControlRed;
+    TextView myTextViewRed;
+
+    SeekBar myControlBlue;
+    TextView myTextViewBlue;
+
     static long prevtime = 0; // for FPS calculation
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +51,18 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // keeps the screen from turning off
 
         mTextView = (TextView) findViewById(R.id.cameraStatus);
+
+        myControlRed = (SeekBar) findViewById(R.id.seekRed);
+        myControlBlue = (SeekBar) findViewById(R.id.seekBlue);
+
+        myTextViewRed = (TextView) findViewById(R.id.textViewRed);
+        myTextViewRed.setText("Adjust Red Sensitivty!");
+
+        myTextViewBlue = (TextView) findViewById(R.id.textViewBlue);
+        myTextViewBlue.setText("Adjust Blue Sensitivity");
+
+        setMyControlListenerRed();
+        setMyControlListenerBlue();
 
         // see if the app has permission to use the camera
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
@@ -64,6 +82,48 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
             mTextView.setText("no camera permissions");
         }
 
+    }
+    private void setMyControlListenerRed() {
+        myControlRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            int progressChangedRed = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedRed = progress;
+                myTextViewRed.setText("Red Sensitivity: "+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+    private void setMyControlListenerBlue() {
+        myControlBlue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            int progressChangedBlue = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedBlue = progress;
+                myTextViewBlue.setText("Blue Sensitivity: "+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
@@ -98,37 +158,62 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
         // every time there is a new Camera preview frame
         mTextureView.getBitmap(bmp);
 
-        final Canvas c = mSurfaceHolder.lockCanvas();
-        if (c != null) {
-            int thresh = 0; // comparison value
-            int[] pixels = new int[bmp.getWidth()]; // pixels[] is the RGBA data
-            int startY = 200; // which row in the bitmap to analyze to read
-            bmp.getPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
+        for (int j = 0; j < bmp.getHeight(); j+=32) {
+            final Canvas c = mSurfaceHolder.lockCanvas();
+            if (c != null) {
+                int threshRed = myControlRed.getProgress(); // comparison value
+                int threshBlue = myControlBlue.getProgress();
+                int[] pixels = new int[bmp.getWidth()]; // pixels[] is the RGBA data
+                int startY = j; // which row in the bitmap to analyze to read
+                bmp.getPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
 
-            // in the row, see if there is more green than red
-            for (int i = 0; i < bmp.getWidth(); i++) {
-                if ((green(pixels[i]) - red(pixels[i])) > thresh) {
-                    pixels[i] = rgb(0, 255, 0); // over write the pixel with pure green
+                // in the row, see if there is more green than red
+                for (int i = 0; i < bmp.getWidth(); i++) {
+                    if (((green(pixels[i]) - red(pixels[i])) > threshRed) && (green(pixels[i]) - blue(pixels[i]) > threshBlue)) {
+                        pixels[i] = rgb(0, 255, 0); // over write the pixel with pure green
+                    }
                 }
-            }
 
-            // update the row
-            bmp.setPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
+                // update the row
+                bmp.setPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
+            }
         }
 
         // draw a circle at some position
-        int pos = 50;
-        canvas.drawCircle(pos, 240, 5, paint1); // x position, y position, diameter, color
+        //int pos = 50;
+        //canvas.drawCircle(pos, 240, 5, paint1); // x position, y position, diameter, color
 
         // write the pos as text
-        canvas.drawText("pos = " + pos, 10, 200, paint1);
-        c.drawBitmap(bmp, 0, 0, null);
-        mSurfaceHolder.unlockCanvasAndPost(c);
+        //canvas.drawText("pos = " + pos, 10, 200, paint1);
+        //c.drawBitmap(bmp, 0, 0, null);
+        //mSurfaceHolder.unlockCanvasAndPost(c);
 
         // calculate the FPS to see how fast the code is running
         long nowtime = System.currentTimeMillis();
         long diff = nowtime - prevtime;
         mTextView.setText("FPS " + 1000 / diff);
         prevtime = nowtime;
+    }
+
+    private void setMyControlListener() {
+        myControlRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            int progressChanged = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+                myTextViewRed.setText("The value is: "+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
